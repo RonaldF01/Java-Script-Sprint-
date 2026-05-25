d25958cf8f47d4dcd55b79f3f41b45068e90dca3
RonaldF01 <ronaldfca3212@gmail.com>
2026-05-25 16:15:50 -0300

    estruturando função para armazenar informações do usuário ao enviar no formulário

--- Java SCRIPT sprint/js/form.js
+++ Java SCRIPT sprint/js/form.js
@@ -1,27 +1,34 @@
-
-//class contato
-
-class contato {
-    
+// Classe de contato
+class Contato {
+    constructor(nome, email, telefone, contato, mensagem) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.contato = contato;
+        this.mensagem = mensagem;
+    }
 }
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
-}
+    // Impede a página de recarregar
+    event.preventDefault();
 
-function Enviar() {
+    // Captura os dados do formulário
+    let data = new Contato(
+        form.elements.namedItem("nome").value,
+        form.elements.namedItem("email").value,
+        form.elements.namedItem("telefone").value,
+        form.elements.namedItem("contato").value,
+        form.elements.namedItem("mensagem").value
+    );
 
-    var nome = document.getElementById("nomeid");
+    // Mostra os dados no console
+    console.log(data);
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
+    // Alerta visual
+   alert("Obrigado " + data.nome + ", sua mensagem foi enviada com sucesso!");
 
+    // Limpa TODOS os campos do formulário
+    form.reset();
 }
